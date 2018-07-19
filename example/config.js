50bf88b1292ca8b1f292e586471689a65a7be479
CorentinLemaitre <corentinlemaitre@gmail.com>
2018-07-19 13:27:00 +0200

    Build geodeeplink V2 and prepare transfert folder

---
+++ example/config.js
@@ -0,0 +1,3 @@
+var config = {
+    accessToken: 'pk.eyJ1IjoiY29yZW50aW5sZW1haXRyZSIsImEiOiJjajQ0OGxmZ2sxNTVkMnFwbjI2M3RsMTc5In0.35Ah1wy069n6jeaniWxMWg'
+};
--- geodeeplink/config.js
+++
@@ -1,3 +0,0 @@
-var config = {
-    accessToken: 'pk.eyJ1IjoiY29yZW50aW5sZW1haXRyZSIsImEiOiJjajQ0OGxmZ2sxNTVkMnFwbjI2M3RsMTc5In0.35Ah1wy069n6jeaniWxMWg'
-};
