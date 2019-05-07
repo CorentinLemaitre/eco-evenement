f1852d3028a95f0d7fccc5cea066139e647248d5
Corentin LEMAITRE <corentinlemaitre@gmail.com>
2019-05-07 19:45:20 +0200

    Finalisation de Héron Voyageur
    Correction du changement de taille,
    Correction de la couche appellé par le popup

--- 2017/config.js
+++ 2017/config.js
@@ -1,3 +1,3 @@
 var config = {
-    accessToken: 'pk.eyJ1IjoiY29yZW50aW5sZW1haXRyZSIsImEiOiJjajQ0OGxmZ2sxNTVkMnFwbjI2M3RsMTc5In0.35Ah1wy069n6jeaniWxMWg'
+    accessToken: 'pk.eyJ1IjoiY29yZW50aW5sZW1haXRyZSIsImEiOiJjamJmMTkwNjMxeHZtMnBteGdxejlscmtqIn0.vO1AFeimgoh60XWlYRzVMw'
 };
